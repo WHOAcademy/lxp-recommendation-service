c2260242203ebca59267670567039ae3ec3ca567
Mahendran Kannan <gkm09@hotmail.com>
2020-08-31 19:11:00 +0530

    🕵  FIX - failing tests in jenkins 🕵

--- lxp_recommendation_service/settings/test.py
+++ lxp_recommendation_service/settings/test.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'bastian',
+        'PASSWORD': 'balthazar',
+        'HOST': os.getenv('TEST_DATABASE_SERVICE_HOST'),
+        'PORT': os.getenv('TEST_DATABASE_SERVICE_PORT'),
+    }
+}
