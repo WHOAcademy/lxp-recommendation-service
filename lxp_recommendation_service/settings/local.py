d501f1779f93f54cfbfb9fc85d0d868b0fd82e65
Frederick Permantier <fpe@redhat.com>
2020-09-01 17:46:38 +0200

    ⛄ use the test-lxp-course-service url ⛄

--- lxp_recommendation_service/settings/local.py
+++ lxp_recommendation_service/settings/local.py
@@ -1,4 +1,4 @@
 from .base import *
 
-LXP_COURSE_SERVICE = 'https://feature-get-course-details-lxp-course-service-labs-dev.apps.who.emea-2.rht-labs.com'
+LXP_COURSE_SERVICE = 'https://test-lxp-course-service-labs-test.apps.who.emea-2.rht-labs.com'
 LXP_PROFILE_SERVICE = 'https://lxp-profile-service-labs-staging.apps.who.emea-2.rht-labs.com'
