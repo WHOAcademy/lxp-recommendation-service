27b0770b75cbfff377a809dcb0603d9a1ad4c983
Frederick Permantier <fpe@redhat.com>
2020-09-09 11:02:05 +0200

    📬 FIX remove hard-coded keycloak Id  📬

--- lxp_recommendation_service/settings/local.py
+++ lxp_recommendation_service/settings/local.py
@@ -1,4 +1,4 @@
 from .base import *
 
-LXP_COURSE_SERVICE = 'https://test-lxp-course-service-labs-test.apps.who.emea-2.rht-labs.com'
-LXP_PROFILE_SERVICE = 'https://test-lxp-profile-service-labs-test.apps.who.emea-2.rht-labs.com'
+LXP_COURSE_SERVICE = 'https://test-lxp-course-service-labs-test.apps.who.lxp.academy.who.int'
+LXP_PROFILE_SERVICE = 'https://test-lxp-profile-service-labs-test.apps.who.lxp.academy.who.int'
--- recommendation_app/tests/test_views.py
+++ recommendation_app/tests/test_views.py
@@ -1,10 +1,29 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from django.conf import settings
+
+import requests
 
 class TestRecommendationView(APITestCase):
 
+    def get_valid_keycloak_id(self):
+        # Get a valid keycloak id
+        # https://test-lxp-profile-service-labs-test.apps.who.lxp.academy.who.int/api/v1/profiles
+        profile_service_url = settings.LXP_PROFILE_SERVICE + '/api/v1/profiles'
+
+        profile_service_response = requests.get(profile_service_url)
+        if profile_service_response.status_code != requests.codes.ok:
+            return None
+
+        profiles = profile_service_response.json()
+
+        for profile in profiles:
+            return  profile['keycloak_id']
+
     def test_get_all(self):
-        url = reverse("list-recommendations", args=['ce395dc2-f89b-44ce-9e12-20d4e99f3d6a'])
+        keycloak_id = self.get_valid_keycloak_id()
+
+        url = reverse("list-recommendations", args=[keycloak_id])
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.data) # checking at least one course is recommended
